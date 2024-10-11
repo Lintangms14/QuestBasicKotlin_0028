b7216a91d6ec57f1f60792990772e5439d2b8b46
Lintangms14 <merayusukmalintang@gmail.com>
2024-10-12 01:12:57 +0700

    untuk menambahkan kelas

--- app/src/androidTest/java/com/example/myapplication/TipeData.kt
+++ app/src/androidTest/java/com/example/myapplication/TipeData.kt
@@ -5,3 +5,30 @@ fun main() {
     val byteValue: Byte = 10
     println("Byte: $byteValue")
 
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    val longValue: Long = 40
+    println("Long: $longValue")
+
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin"
+    println("String: $stringValue")
+}
