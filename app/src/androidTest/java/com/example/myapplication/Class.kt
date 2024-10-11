a57e908afc2e5b6f3079b14d2567303957be358b
Lintangms14 <merayusukmalintang@gmail.com>
2024-10-12 01:17:23 +0700

    untuk mengfixkan file kelas

---
+++ app/src/androidTest/java/com/example/myapplication/Class.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+class Motor ()
+
+class Contact(val id: Int, var email: String)
+
+
+// Fungsi main harus berada di luar kelas
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+    // Print the value of the property: email
+    println(contact.email)
+
+    // Updates the value of the property: email
+    contact.email = "jane@gmail.com"
+
+    // Print the new value of the property: email
+    println(contact.email)
+}
